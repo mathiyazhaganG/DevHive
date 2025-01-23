e379b1eac54a2dd726b3869461fb6c26fd4cf321
unknown <mathiyazhagantvm@gmail.com>
2025-01-23 22:22:09 +0530

    database & api validation

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.post("/signup", async (req, res) => {
         res.status(201).json({ message: "User created" });
     } catch (error) {
         console.error("Error creating user:", error);
-        res.status(500).json({ message: "Something went wrong" });
+        res.status(400).json({ message: error.message });
     }
 });
 
@@ -41,11 +41,18 @@ app.delete("/user", async (req, res) => {
     }
 });
 
-app.patch("/user", async (req, res) => {
-    const email = req.body.emailId;
-    const lname = req.body.lastName;
+app.patch("/user/:emailId", async (req, res) => {
+    const email = req.params.emailId;
+    const data = req.body;
     try {
-        const updatedUser = await User.findOneAndUpdate({ emailId: email }, { lastName: lname });
+        const allowed_updates = ["firstName", "lastName", "password", "gender", "age", "skills"];
+        const isupdated_allowed = Object.keys(data).every((k) => allowed_updates.includes(k));
+        if (!isupdated_allowed) {
+            throw new Error("Update not allowed");
+        }
+        const updatedUser = await User.findOneAndUpdate({ emailId: email }, data, {
+            runValidators: true
+        });
         res.status(200).json({ message: "User updated", updatedUser });
         console.log(updatedUser);
     } catch (error) {
--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,53 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const UserSchema = new mongoose.Schema({
-	firstName: {
-		type: String
-	},
-	lastName: {
-		type: String
-	},
-	emailId: {
-		type: String
-	},
-	password: {
-		type: String
-	},
-	gender: {
-		type: String
-	},
-});
+    firstName: {
+        type: String,
+        required: [true, 'First name is required'],
+        minlength: [3, 'First name must be at least 3 characters long'],
+        maxlength: [50, 'First name must be at most 50 characters long']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name is required'],
+        default: "N/A"
+    },
+    emailId: {
+        type: String,
+        required: [true, 'Email ID is required'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+		validate(value){
+			if(!validator.isEmail(value)){
+				throw new Error('Invalid email');
+		}}
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [4, 'Password must be at least 4 characters long'],
+        maxlength: [100, 'Password must be at most 100 characters long']
+    },
+    gender: {
+        type: String,
+        required: [true, 'Gender is required'],
+        validate(value) {
+            if (!['male', 'female', 'other'].includes(value)) {
+                throw new Error("Gender is not valid");
+            }
+        }
+    },
+    age: {
+        type: Number,
+        required: [true, 'Age is required'],
+        min: [18, 'Age must be at least 18'],
+        max: [100, 'Age must be at most 100']
+    },
+    skills: {
+        type: Array,
+        default: []
+    }
+}, { timestamps: true });
+
 module.exports = mongoose.model('User', UserSchema);
